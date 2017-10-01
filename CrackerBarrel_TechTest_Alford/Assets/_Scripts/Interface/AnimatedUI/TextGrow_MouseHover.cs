255b2f3715a4bdef65b6b660b9af1f325c219d76
Reid Alford <rmalford2007@gmail.com>
2017-10-01 16:17:09 -0600

    Add text hover animations for menu buttons
    Added a hover animation that grow a font by a percent controled by an
    animation curve. When that curve is done and the mouse remains hovering
    there is a pulse animation curve that occurs until the mouse leaves.
    Added an editor drawer for the ui script to modify event trigger class
    for the animation.

---
+++ CrackerBarrel_TechTest_Alford/Assets/_Scripts/Interface/AnimatedUI/TextGrow_MouseHover.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+[System.Serializable]
+public class TextGrow_MouseHover : EventTrigger
+{
+    [Tooltip("This is the additional font size percent we are adding to the starting font size. This plays forward when hover begins, and backwards when leaving the hovered state.")]
+    public AnimationCurve sizeCurve;
+
+    [Tooltip("This is the additional font size percent we are adding to the starting font size plus the hover size curve. This only occurs when the initial hover curve is completed. This should be a faint pulse if the player leaves his mouse over a button.")]
+    public AnimationCurve idlePulseCurve;
+
+    public bool isInteractable;
+    private TMP_Text theText;
+    private float startingFontSize;
+    private float animationCurveTime = 0f;
+    private float elapsedTime = 0f;
+    private bool isForward = false;
+    private Button myButton;
+    // Use this for initialization
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+        if (myButton != null)
+        {
+            isInteractable = myButton.interactable;
+        }
+
+        theText = GetComponent<TMP_Text>();
+        if (theText == null)
+            Destroy(this);
+        startingFontSize = theText.fontSize;
+        animationCurveTime = sizeCurve.keys[sizeCurve.length - 1].time;
+
+    }
+
+    private void OnEnable()
+    {
+        isForward = false;
+        elapsedTime = 0f;
+        if (theText != null)
+            theText.fontSize = startingFontSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInteractable != myButton.interactable)
+        {
+            isInteractable = myButton.interactable;
+        }
+
+        if (isInteractable)
+        {
+            elapsedTime += (Time.unscaledDeltaTime * (isForward ? 1.0f : -1.0f));
+            if ((isForward && elapsedTime < animationCurveTime) || (!isForward && elapsedTime > 0.0f))
+            {
+                elapsedTime = Mathf.Clamp(elapsedTime, 0.0f, animationCurveTime);
+                theText.fontSize = startingFontSize + startingFontSize * sizeCurve.Evaluate(elapsedTime);
+            }
+            else if(isForward && elapsedTime >= animationCurveTime)
+            {
+                theText.fontSize = startingFontSize + startingFontSize * sizeCurve.Evaluate(animationCurveTime) + startingFontSize * idlePulseCurve.Evaluate(elapsedTime - animationCurveTime);
+            }
+        }
+        else
+        {
+            elapsedTime += (Time.unscaledDeltaTime * (-1.0f));
+            if (!isForward && elapsedTime > 0.0f)
+            {
+                elapsedTime = Mathf.Clamp(elapsedTime, 0.0f, animationCurveTime);
+                theText.fontSize = startingFontSize + startingFontSize * sizeCurve.Evaluate(elapsedTime);
+            }
+        }
+    }
+
+    public override void OnPointerEnter(PointerEventData data)
+    {
+        isForward = true;
+    }
+
+    public override void OnPointerExit(PointerEventData data)
+    {
+        isForward = false;
+    }
+}
